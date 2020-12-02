e9c3ea010f41cd99356b50b73dcf0cc267ac12e8
teddy1496 <32216501+teddy1496@users.noreply.github.com>
2020-12-03 00:52:50 +0200

    Update launch_DirectTeleop.sh

--- launch_DirectTeleop.sh
+++ launch_DirectTeleop.sh
@@ -1,8 +1,9 @@
-cd ~/shared_control_python/
+cd ~/shared_control_python_KB/
 source devel/setup.bash
 cd launch/
 
 export SVGA_VGPU10=0
+
 gnome-terminal -- roslaunch MirPoles_World_DT.launch &
 sleep 8
 
@@ -10,3 +11,5 @@ rosservice call /gazebo/unpause_physics
 
 rosservice call /gazebo/set_model_state '{model_state: { model_name: mir, pose: { position: { x: -4, y: -4 ,z: 0 }, orientation: {x: 0, y: 0, z: 0, w: 1 } }, twist: { linear: {x: 0.0 , y: 0 ,z: 0 } , angular: { x: 0.0 , y: 0 , z: 0.0 } } , reference_frame: world } }'
 
+
+gnome-terminal -- rosrun teleop_twist_keyboard teleop_twist_keyboard_DT.py
